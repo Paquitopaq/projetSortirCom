5f57832e7a8a87f787b043dc09b8620b501df0aa
Quentin <plld@hotmail.fr>
2025-10-10 13:24:51 +0100

    mis a niveau dev main

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Form\ProfilType;
 use App\Form\UpdateProfilType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
