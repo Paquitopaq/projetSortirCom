2f84986dfaa303a04512d691c4e30429885ca7f8
cboulesteix2023 <clement.boulesteix2023@campus-eni.fr>
2025-10-10 14:58:35 +0200

    import en trop + image PP

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\ProfilType;
-use App\Form\UpdateProfilType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
