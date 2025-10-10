090b42a4c39d54d65f3b91459f1f74c5315dad37
Orane MERTZ <orane10@live.fr>
2025-10-10 12:08:09 +0200

    modif participant controller/repo

--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -5,12 +5,15 @@ namespace App\Form;
 use App\Entity\Participant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-class UpdateProfilType extends AbstractType
+class ProfilType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -20,6 +23,7 @@ class UpdateProfilType extends AbstractType
             ->add('prenom')
             ->add('telephone')
             ->add('email')
+            ->add('administrateur')
             ->add('photoProfil', FileType::class, [
                 'label' => 'Photo de profil',
                 'mapped' => false,
@@ -31,16 +35,36 @@ class UpdateProfilType extends AbstractType
                         'mimeTypesMessage' => 'Formats autorisés : JPG, PNG',
                     ])
                 ],
-            ])
-            ->add('submit', SubmitType::class, [
-        'label' => 'Sauvegarder',
-    ]);
+            ]);
+        if ($options['is_create']) {
+            $builder->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+        }
+        $builder->add('submit', SubmitType::class, [
+            'label' => 'Sauvegarder'
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Participant::class,
+            'is_create' => false,
         ]);
     }
 }
