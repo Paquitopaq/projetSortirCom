e1778104a75cd5487d4ec3411723488e830b96ea
cboulesteix2023 <clement.boulesteix2023@campus-eni.fr>
2025-10-13 14:38:25 +0200

    Restore dev partie 2

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -24,7 +24,7 @@ class RegistrationController extends AbstractController
             /** @var string $plainPassword */
             $plainPassword = $form->get('plainPassword')->getData();
 
-            // encode the plain password
+            // encode the plain mot de pass
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $entityManager->persist($user);
