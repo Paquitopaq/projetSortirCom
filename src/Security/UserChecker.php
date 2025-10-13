12157fae6540183c6431f78548ff12dea9174dee
Orane MERTZ <orane10@live.fr>
2025-10-13 11:25:51 +0200

    security (et un peu de responsive)

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Participant;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof Participant) {
+            return;
+        }
+
+        if (!$user->getActif()) {
+            throw new CustomUserMessageAccountStatusException('Votre compte a été désactivé.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {}
+}
