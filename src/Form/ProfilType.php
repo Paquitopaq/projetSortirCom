f0126f8fd0fcad441dfc662b3f584b83677e2983
cboulesteix2023 <clement.boulesteix2023@campus-eni.fr>
2025-10-16 14:35:10 +0200

    Fix photo et password obligatoire à l'update

--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -41,10 +41,8 @@ class ProfilType extends AbstractType
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'required' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
